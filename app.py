faa8f995678db2eb341e00fe9a1f3caec64efce4
KurtMaiquez <112041100+KurtMaiquez@users.noreply.github.com>
2023-12-10 08:52:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import tensorflow as tf
 import streamlit as st
 import numpy as np
@@ -12,15 +11,25 @@ st.title("Image Classification App")
 # Upload an image through Streamlit
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-if uploaded_file is not None:
-    # Preprocess the image
-    image = tf.keras.preprocessing.image.load_img(uploaded_file, target_size=(32, 32))
-    image_array = tf.keras.preprocessing.image.img_to_array(image)
-    image_array = tf.expand_dims(image_array, 0)  # Create a batch
+try:
+    if uploaded_file is not None:
+        # Preprocess the image
+        image = tf.keras.preprocessing.image.load_img(uploaded_file, target_size=(32, 32))
+        image_array = tf.keras.preprocessing.image.img_to_array(image)
+        image_array = tf.expand_dims(image_array, 0)  # Create a batch
 
-    # Make predictions
-    predictions = model.predict(image_array)
-    score = tf.nn.softmax(predictions[0])
+        # Normalize the image (assuming the model was trained with normalized input)
+        image_array /= 255.0
 
-    # Display the results
-    st.image(image, caption=f"Predicted class: {np.argmax(score)}, Confidence: {100 * np.max(score):.2f}%")
+        # Make predictions
+        predictions = model.predict(image_array)
+        top_classes = tf.argsort(predictions[0], direction='DESCENDING')[:3]  # Display top 3 predictions
+        top_scores = tf.nn.softmax(predictions[0][top_classes])
+
+        # Display the results
+        st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.write("Top Predictions:")
+        for i, (class_idx, score) in enumerate(zip(top_classes, top_scores)):
+            st.write(f"{i + 1}. Class: {class_idx}, Confidence: {100 * score:.2f}%")
+except Exception as e:
+    st.write("An error occurred:", str(e))
